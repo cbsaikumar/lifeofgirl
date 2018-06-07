3b6685bc4a49e7fd5809ded0933d50bd80bc6d6c
Bhargava Sai Kumar <cbsaikumar@gmail.com>
2018-06-07 14:44:37 +0530

    third commit linked mlab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost/intern_management_system',
+    //database: 'mongodb://localhost/intern_management_system',
+    database : 'mongodb://cbsaikumar:Apple@123@ds151840.mlab.com:51840/lifeofgirl',
     secret: "yoursecret"
 }
